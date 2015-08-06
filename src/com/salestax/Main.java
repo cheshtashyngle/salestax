fb992dcd62f5b6ad5655721c18ab88c312dc263f
Cheshta <cheshu_25@yahoo.co.in>
2015-08-06 10:48:22 +0530

    [Cheshta] [#0002] Applying lexer on the input

--- src/com/salestax/Main.java
+++ src/com/salestax/Main.java
@@ -3,6 +3,16 @@ package com.salestax;
 public class Main {
 
     public static void main(String[] args) {
-        
+        Console console = new Console();
+        while(true) {
+            String input = console.input();
+            if(input.equals("Quit"))
+                break;
+            String[] inputs = input.split(" at ");
+            double rate = Double.parseDouble(inputs[1]);
+            inputs = inputs[0].split(" ",2);
+            int number = Integer.parseInt(inputs[0]);
+            String item = inputs[1];
+        }
     }
 }
