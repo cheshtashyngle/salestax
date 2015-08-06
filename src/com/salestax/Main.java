5ff0520948f8f6afbd28aacbbd9abda5b70fcf69
Cheshta <cheshu_25@yahoo.co.in>
2015-08-06 11:10:18 +0530

    [Cheshta] [#0003] Determing whether the item is imported or not and belongs to which category

--- src/com/salestax/Main.java
+++ src/com/salestax/Main.java
@@ -1,9 +1,20 @@
 package com.salestax;
 
+import java.util.ArrayList;
+
 public class Main {
 
     public static void main(String[] args) {
         Console console = new Console();
+        ArrayList<String> food = new ArrayList<String>();
+        food.add("chocolate bar");
+        food.add("box of imported chocolates");
+        food.add("box of imported chocolates");
+        food.add("imported box of chocolates");
+        ArrayList<String> books = new ArrayList<String>();
+        books.add("book");
+        ArrayList<String> medicines = new ArrayList<String>();
+        medicines.add("packet of headache pills");
         while(true) {
             String input = console.input();
             if(input.equals("Quit"))
@@ -13,6 +24,20 @@ public class Main {
             inputs = inputs[0].split(" ",2);
             int number = Integer.parseInt(inputs[0]);
             String item = inputs[1];
+            String category;
+            if(food.contains(item))
+                category = "food";
+            else if(books.contains(item))
+                category = "book";
+            else if(medicines.contains(item))
+                category = "medicine";
+            else
+                category = "others";
+            boolean imported;
+            if(item.contains("imported"))
+                imported = true;
+            else
+                imported = false;
         }
     }
 }
